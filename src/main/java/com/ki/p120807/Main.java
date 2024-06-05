5c8594e3cadd3457bb39db338cea8e490ed2bad1
es1210 <dmstn27034@gmail.com>
2024-06-05 09:59:46 +0900

    프로젝트ì 세팅

---
+++ src/main/java/com/ki/p120807/Main.java
@@ -0,0 +1,15 @@
+package com.ki.level0.p120807;
+
+public class Main{
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return answer;
+    }
+}
